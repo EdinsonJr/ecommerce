37a43ae1947b19aa8525df450540c16666882d69
EdinsonJr <edinsonacostajr@gmail.com>
2023-04-29 01:14:37 -0500

    le puse la animacion al navbar con js

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,13 @@
+const hola = document.querySelector("header");
 
+function animarNav() {
+    let scroll = window.scrollY
+    if (scroll > 80) {
+        hola.classList.add("navbar-scroll")
+    }
+    else{
+        hola.classList.remove("navbar-scroll")
+    }
+}
 
+window.onscroll = () => animarNav();
