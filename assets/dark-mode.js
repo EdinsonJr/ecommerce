d98b5060780d14800d145ad13bfda7fe706e25aa
EdinsonJr <edinsonacostajr@gmail.com>
2023-04-29 21:48:30 -0500

    le puse el cambio de color de texto al navbar, y agregue los iconos de darkmode y shopping

---
+++ assets/dark-mode.js
@@ -0,0 +1,5 @@
+const moon = document.querySelector(".icons:nth-child(1)");
+
+    moon.addEventListener('click',function () {
+        moon.classList.add("dark-mode")
+    })
--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,29 @@
+const home = document.querySelector('ul > li:nth-child(1)')
+const products = document.querySelector('ul > li:nth-child(2)');
 const hola = document.querySelector("header");
 
 function animarNav() {
-    let scroll = window.scrollY
+    let scroll = window.scrollY;
     if (scroll > 80) {
-        hola.classList.add("navbar-scroll")
+        hola.classList.add("navbar-scroll");
     }
     else{
-        hola.classList.remove("navbar-scroll")
+        hola.classList.remove("navbar-scroll");
     }
+    if (scroll <= 450 || scroll === 0) {
+        home.classList.add("principal");
+    } else {
+        home.classList.remove("principal")
+    }
+    if (scroll >= 450) {
+        products.classList.add("principal")
+    }
+    else{
+        products.classList.remove("principal")
+    }
+    console.log(scroll);
 }
 
 window.onscroll = () => animarNav();
+
+
