3cb1d11b8bb086313aef63e7b241ce47cd40647e
EdinsonJr <edinsonacostajr@gmail.com>
2023-05-03 00:20:57 -0500

    agregando dark-mode

--- assets/dark-mode.js
+++
@@ -1,5 +0,0 @@
-const moon = document.querySelector(".icons:nth-child(1)");
-
-    moon.addEventListener('click',function () {
-        moon.classList.add("dark-mode")
-    })
--- assets/main.js
+++ assets/main.js
@@ -27,3 +27,12 @@ function animarNav() {
 window.onscroll = () => animarNav();
 
 
+const dark = document.querySelector("#moon-dark");
+dark.addEventListener("click", function() {
+document.body.classList.toggle("dark-mode");
+if (document.body.classList.contains("dark-mode")) {
+    dark.src ="./assets/img/bx-sun.png";
+} else {
+    dark.src ="./assets/img/bx-moon.png";
+}
+});
