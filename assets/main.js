6a1babaad7e1e1b811f35b72154fc74e65d3b758
EdinsonJr <edinsonacostajr@gmail.com>
2023-05-04 16:01:32 -0500

    Consumí la api, mejoré el filtrado y le agregué el plus"(+)" a todos los items, y elimine archivos innecesarios

--- assets/main.js
+++ assets/main.js
@@ -31,3 +31,51 @@ const dark = document.querySelector("#moon-dark");
 dark.addEventListener("click", function() {
 document.body.classList.toggle("dark-mode")
 });
+
+async function peticion(){
+    try {
+        const data = await fetch("https://ecommercebackend.fundamentos-29.repl.co/");
+        const ecommerce = await data.json();
+        return ecommerce;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function main() {
+    const ecomme = await peticion()
+    let html='';
+    for (const { id,name,category,image,price,quantity } of ecomme) {
+        
+        html += `
+        <div class ="description ${category} id="${id}">
+        <div class="description-img">
+                        <img src="${image}" alt="${name}">
+                    </div>
+                    <div class="description-info">
+                    <div class="bx-plus"> + </div>
+                        <div class="precio">
+                            <h3>${price}.00</h3>
+                            <p>Stock: ${quantity}</p>
+                        </div>
+                            <p>${name}</p>
+                    </div>
+                    </div>
+        `
+        
+    }
+    
+    document.querySelector(".items").innerHTML = html;
+
+    mixitup(".items",{
+        selectors:{
+            target:'.description'
+        },
+        animation:{
+            duration:300
+        } 
+            
+    }).filter(all)
+}
+
+main();
